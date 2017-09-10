d830c35011fda7ff515a63ce3a5acf53c4f76f72
elias <elias.alhomsi@mail.mcgill.ca>
2017-09-09 23:22:37 -0400

    add files

--- solution.py
+++ solution.py
@@ -12,23 +12,40 @@ import sys #for arguments
 
 #define main (this is an inner join)
 def main(argv):
-	if(len(argv) != 3):
-		print("USAGE: " + argv[0] + " <column1> <column2> \n Those two corresponds two url1 and url2 respectivly")
-	
+	if not (len(argv) in [3, 5]):
+		print("USAGE: " + argv[0] + " <column1> <column2> [<file1>] [<file2>] \n Those two corresponds two url1/file1 and url2/file2 respectivly \n note that if files were not specified the program will use the default url")
+		exit(1)
+
 	col1 = argv[1];
 	col2 = argv[2];
 
+	customers = None
+	orders = None
 
-	#fetch the first url
-	url1 = "https://gist.githubusercontent.com/udnay/d8e2ea75f2cfd7d75482f42549c31c59/raw/60da021e9f083f0c4bf0910f690baf5f38410bc6/customers.json"
-	response = urllib.urlopen(url1)
-	customers = json.loads(response.read())
+	if(len(argv) == 3):
+		#fetch the first url
+		url1 = "https://gist.githubusercontent.com/udnay/d8e2ea75f2cfd7d75482f42549c31c59/raw/60da021e9f083f0c4bf0910f690baf5f38410bc6/customers.json"
+		response = urllib.urlopen(url1)
+		customers = json.loads(response.read())
 
-	#fetch the second url
-	url2 = "https://gist.githubusercontent.com/udnay/20603ff9956064c8d1f1abf7a5e6f5b2/raw/9e841b973a3d9d51940bdffe162c1400a9bac022/orders.json"
-	response = urllib.urlopen(url2)
-	orders = json.loads(response.read())
+		#fetch the second url
+		url2 = "https://gist.githubusercontent.com/udnay/20603ff9956064c8d1f1abf7a5e6f5b2/raw/9e841b973a3d9d51940bdffe162c1400a9bac022/orders.json"
+		response = urllib.urlopen(url2)
+		orders = json.loads(response.read())
 
+
+	elif(len(argv) == 5):
+		file1= argv[3]
+		file2= argv[4]
+
+		with open(file1) as json_data:
+			customers = json.load(json_data)
+	
+		with open(file2) as json_data:
+			orders = json.load(json_data)
+		
+
+    #result array
 	result = list()
 
 	#count barry and steve while building the solution
@@ -44,7 +61,7 @@ def main(argv):
 					steveCounter+=1
 
 	print(json.dumps(result))
-
+	print("the length of the resulting array is " + str(len(result)))
 	print("\nbarry counter is " + str(barryCounter))
 	print("steve counter is " + str(steveCounter))
 
